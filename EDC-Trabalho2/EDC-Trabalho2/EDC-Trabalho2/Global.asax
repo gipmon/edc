61588b5fbd1b2eaa0aba63ca6ebf7cc40f50ba43
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-10-14 09:58:40 +0100

    edc trabalho2

---
+++ EDC-Trabalho2/EDC-Trabalho2/EDC-Trabalho2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EDC_Trabalho2.Global" Language="C#" %>
---
+++ EDC-Trabalho2/EDC-Trabalho2/EDC-Trabalho2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EDC_Trabalho2.Startup))]
+namespace EDC_Trabalho2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
