2bbcb5cd4dae8b6c0e1263c6d120a990b54a3ae7
Rafael Ferreira <mail@rafaelferreira.pt>
2015-12-05 19:59:27 +0000

    rss init

---
+++ EDC - Trabalho Final/FootballData/FootballData/rss.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Language="C#" ContentType="text/xml" AutoEventWireup="true" CodeBehind="rss.aspx.cs" Inherits="NettutsTutorial2.rss" %>
+<asp:Repeater ID="RepeaterRSS" runat="server">
+</asp:Repeater>
---
+++ EDC - Trabalho Final/FootballData/FootballData/rss.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FootballData
+{
+    public partial class rss : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected string RemoveIllegalCharacters(object input)
+        {
+            // cast the input to a string
+            string data = input.ToString();
+
+            // replace illegal characters in XML documents with their entity references
+            data = data.Replace("&", "&amp;");
+            data = data.Replace("\"", "&quot;");
+            data = data.Replace("'", "&apos;");
+            data = data.Replace("<", "&lt;");
+            data = data.Replace(">", "&gt;");
+
+            return data;
+        }
+    }
+}
